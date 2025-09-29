e1e38a444b7d43bbcc01fbc4e43689f0ee0f0330
Totes <150313225+TotesMan@users.noreply.github.com>
2025-09-29 18:49:53 +0800

    Update toDoApp.py
    Added documentation, and fixed indentations

--- toDoApp.py
+++ toDoApp.py
@@ -1,41 +1,49 @@
 # toDoApp.py
 
+#Initialize Task list
 tasks=[]
 
-def addtask(task) : #Alli
-  tasks.append(task)
-  print("task added!")
+# Adds a Task to the list
+def add_Task(task):
+    tasks.append(task)
+    print("Task added!")
 
-def showTasks( ): #CB
-    if len(tasks)==0 :
-      print("no tasks yet")
+# shows a Task
+def show_Tasks():
+    if len(tasks) == 0:
+        print("No tasks yet.")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i in range (len(tasks)):
+            print(i + 1, ".", tasks[i])
 
-def removetask(tasknumber): #Jaspur
-    tasks.pop(tasknumber) 
-    print("task removed!!")
+# Deletes a Task (Logic does not work, deletes recent task instead of the selected task)
+def remove_Task(tasknum):
+    tasks.pop(tasknum) 
+    print("Task has been removed!!")
 
 def main():
-  # Totes
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
-        print("4- Exit")
-        ch = input("enter choice : ")
-        if ch=="1":
-            t = input("enter task : ")
-            addtask(t)
-        elif ch=="2":
-            showTasks()
-        elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+        print("1. Add a Task")
+        print("2. Show Tasks")
+        print("3. Remove a Task")
+        print("4. Exit")
+        chc = input("enter choice: ")
+        
+        if chc == "1":
+            task = input("Enter task: ")
+            add_Task(task)
+            
+        elif chc == "2":
+            show_Tasks()
+            
+        elif chc == "3":
+            num = int(input("Enter task no. to remove: "))
+            remove_Task(num)  
+             
+        elif chc == "4":
+            break
+        
         else:
-            print("wrong choice!!")
+            print("Please input a valid choice!")
+            
 main()
-
